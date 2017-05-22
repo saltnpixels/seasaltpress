14b8654fcb3a46014cdb8608821510d4eb62dddc
= <=>
2017-05-22 15:26:27 -0400

    Small fixes

--- inc/seasaltpress_custom_fields.php
+++ inc/seasaltpress_custom_fields.php
@@ -117,10 +117,12 @@ class Rational_Meta_Box {
 	 */
 	public function admin_footer() {
 		
+		
 		?><script>
 			// https://codestag.com/how-to-use-wordpress-3-5-media-uploader-in-theme-options/
 			jQuery(document).ready(function($){
-				if ( typeof wp.media !== 'undefined' ) {
+				
+				if ( typeof wp.media !== 'undefined' && typeof wp.media.editor !== 'undefined') {
 					var _custom_media = true,
 					_orig_send_attachment = wp.media.editor.send.attachment;
 					$('.rational-metabox-media').click(function(e) {
