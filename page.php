e4c0f6710b02e0107b020091c24e42d2ac748f17
= <=>
2017-05-26 09:49:37 -0400

    Revert "formatted nicer"
    This reverts commit 1800f573e5d38f4439bee88b93b31b773e1e2747.

--- page.php
+++ page.php
@@ -18,23 +18,24 @@
 get_header(); ?>
 
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-        <?php
-        while ( have_posts() ) : the_post();
+			<?php
+			while ( have_posts() ) : the_post();
+		
+			include( locate_template( 'template-parts/page/content.php' ) );
+		
 
-        include( locate_template( 'template-parts/page/content.php' ) );
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
+			endwhile; // End of the loop.
+			?>
 
-        endwhile; // End of the loop.
-        ?>
-
-    </main><!-- #main -->
-</div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_footer();
